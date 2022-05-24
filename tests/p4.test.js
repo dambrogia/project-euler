9222942df12c6df552a14e162bdd936ad52be8ca
Dominic dambrogia <dominic.dambrogia@palmettostatearmory.com>
2022-05-23 17:21:42 -0700

    stub test 4

---
+++ tests/p4.test.js
@@ -0,0 +1,18 @@
+/**
+A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+
+Find the largest palindrome made from the product of two 3-digit numbers.
+ * @param Number input
+ * @returns Number
+ */
+const execute = n => {
+
+};
+
+test('test base case problem 4', () => {
+    expect(true).toBe(false);
+});
+
+test('test use case problem 4', () => {
+    expect(true).toBe(false);
+});
