9914b9e4bfddc23f6ac42e70d3d27dacc84d6c53
Dominic dambrogia <dominic.dambrogia@palmettostatearmory.com>
2022-05-23 18:32:28 -0700

    stub test 6

---
+++ tests/p6.test.js
@@ -0,0 +1,22 @@
+/**
+The sum of the squares of the first ten natural numbers is,
+
+The square of the sum of the first ten natural numbers is,
+
+Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is .
+
+Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+ * @param Number n
+ * @returns Number
+ */
+const execute = n => {
+
+};
+
+test('test base case problem 6', () => {
+    expect(execute(10)).toBe(2640);
+});
+
+test('test use case problem 6', () => {
+    expect(execute(100)).toBe('?');
+});
