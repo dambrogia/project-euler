aa9ecf94bf6fdbac8d4658f1e9500026163bb0d3
Dominic dambrogia <dominic.dambrogia@palmettostatearmory.com>
2022-05-23 18:39:53 -0700

    complete problem 6

--- tests/p6.test.js
+++ tests/p6.test.js
@@ -10,7 +10,15 @@ Find the difference between the sum of the squares of the first one hundred natu
  * @returns Number
  */
 const execute = n => {
+    var sumOfSquares = 0;
+    var squareOfSums = 0;
 
+    for (var i = 1; i <= n; i++) {
+        sumOfSquares = Math.pow(i, 2) + sumOfSquares;
+        squareOfSums = squareOfSums + i;
+    }
+
+    return Math.pow(squareOfSums, 2) - sumOfSquares;
 };
 
 test('test base case problem 6', () => {
@@ -18,5 +26,5 @@ test('test base case problem 6', () => {
 });
 
 test('test use case problem 6', () => {
-    expect(execute(100)).toBe('?');
+    expect(execute(100)).toBe(25164150);
 });
