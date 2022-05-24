07818a15ebc40bb15237bad6b177287da59841bc
Dominic dambrogia <dominic.dambrogia@palmettostatearmory.com>
2022-05-23 18:12:38 -0700

    stub test 5

---
+++ tests/p5.test.js
@@ -0,0 +1,18 @@
+/**
+2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+
+What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+ * @param Number n
+ * @returns Number
+ */
+const execute = n => {
+
+};
+
+test('test base case problem 5', () => {
+    expect(10).toBe(2520);
+});
+
+test('test use case problem 5', () => {
+    expect(true).toBe(false);
+});
