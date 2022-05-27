6f5f23d032f11829e972d76139dd82c835c555ca
Dominic dambrogia <dominic.dambrogia@palmettostatearmory.com>
2022-05-27 10:27:45 -0700

    complete problem 8

---
+++ tests/base.js
@@ -0,0 +1,15 @@
+/**
+ * @param mixed n
+ * @returns Number
+ */
+const execute = n => {
+    return false;
+};
+
+test('test base case problem', () => {
+    expect(execute(1)).toBe(true);
+});
+
+test('test use case problem', () => {
+    expect(execute(1).toBe(true);
+});
